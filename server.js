455f8dc294465be89e80d646cf2f5c33eaa1fd3c
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2025-05-16 09:07:35 +0600

    updated by shuvo sarkar

--- server.js
+++ server.js
@@ -18,12 +18,7 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/api/blogs', blogRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
